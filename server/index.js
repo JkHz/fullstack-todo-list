25801804a841f8044378aeba3e764f25510bc5d7
Josh Hertz <jhertz1@gmail.com>
2021-08-17 13:41:01 -0600

    change server port

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = 3001;
+const port = 3000;
 const dbHelpers = require('../database/dbHelpers');
 
 app.use(morgan('dev'));
