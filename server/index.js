28b77794a368ed206cb55807f98e4f2d9e0f75ed
Josh Hertz <jhertz1@gmail.com>
2021-08-17 10:21:24 -0600

    set up server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const cors = require('cors');
+const app = express();
+const port = 3001;
+
+app.use(morgan('dev'));
+app.use(express.json());
+// app.use(cors());
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // TODO:
 //   1. Create instance of express
@@ -8,3 +17,6 @@ const path = require('path');
 //   3. Serve static files in 'dist' directory
 //   4. Create request handlers for all CRUD operations
 //   5. Launch server on a port of your choosing
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
